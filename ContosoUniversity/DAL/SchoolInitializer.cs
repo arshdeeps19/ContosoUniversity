085af38bf67087c2e8400a9a0166a6cb20e4ee40
arshdeepsingh <arshdeepsingh@ARSHDEEPSINA1B3>
2024-07-10 15:49:15 -0700

    Add project files.

---
+++ ContosoUniversity/DAL/SchoolInitializer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using ContosoUniversity.Models;
+
+namespace ContosoUniversity.DAL
+{
+    public class SchoolInitializer : DropCreateDatabaseIfModelChanges<SchoolContext>
+    {
+        protected override void Seed(SchoolContext context)
+        {
+            var students = new List<Student>
+            {
+                new Student{FirstMidName="Carson",LastName="Alexander",EnrollmentDate=DateTime.Parse("2005-09-01")},
+                // Add more students
+            };
+
+            students.ForEach(s => context.Students.Add(s));
+            context.SaveChanges();
+
+            var courses = new List<Course>
+            {
+                new Course{CourseID=1050,Title="Chemistry",Credits=3,},
+                // Add more courses
+            };
+            courses.ForEach(s => context.Courses.Add(s));
+            context.SaveChanges();
+
+            var enrollments = new List<Enrollment>
+            {
+                new Enrollment{StudentID=1,CourseID=1050,Grade=Grade.A},
+                // Add more enrollments
+            };
+            enrollments.ForEach(s => context.Enrollments.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
